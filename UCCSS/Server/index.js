5890bb6b1f81c85a3584e4904d05a0478ecc83d4
Osman <erenay.osman@gmail.com>
2018-10-08 18:16:03 -0500

    More routes added

--- UCCSS/Server/index.js
+++ UCCSS/Server/index.js
@@ -1,9 +1,34 @@
+var http = require('http');
 var express = require('express');
 var app = express();
-app.set('port', process.env.PORT || 3000);
+
+app.use(function(req, res, next){
+    console.log('Request from ' + req.ip);
+    next();
+});
+
 app.get('/',function(req,res){
     res.send('Hello World!');
 });
-app.listen(app.get('port'), function(){
-    console.log('Express started on http://localhost:' + app.get('port'));
+
+app.get('/about',function(req,res){
+    res.send('About Us!!');
 });
+app.get('/about/directions',function(req,res){
+    res.send('How to find Us!');
+});
+
+app.use(function(req, res){       //THESE TWO SHOUL BE AT THE END.. 404 and 500 errors
+    res.type('text/plan');
+    res.status(404);
+    res.send('404 Not Found');
+});
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plan');
+    res.status(500);
+    res.send('500 Sever Error');
+});
+http.createServer(app).listen(3000, function(){
+    console.log('Express server listening on port ' + 3000);
+})

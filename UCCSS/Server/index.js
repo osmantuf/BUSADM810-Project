a52b7ff8f877aca8b1773b7c936230aced9a49bc
Osman <erenay.osman@gmail.com>
2018-10-08 18:02:34 -0500

    Express server and Hello world route created

---
+++ UCCSS/Server/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+app.get('/',function(req,res){
+    res.send('Hello World!');
+});
+app.listen(app.get('port'), function(){
+    console.log('Express started on http://localhost:' + app.get('port'));
+});

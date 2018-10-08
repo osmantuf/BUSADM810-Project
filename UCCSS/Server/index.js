897b2ef3ab39b0100d6ccffb5f51972de063d6d1
Osman <erenay.osman@gmail.com>
2018-10-08 18:29:57 -0500

    config file added

--- UCCSS/Server/index.js
+++ UCCSS/Server/index.js
@@ -1,34 +1,12 @@
-var http = require('http');
 var express = require('express');
+var config = require('./config/config');
+
 var app = express();
 
-app.use(function(req, res, next){
-    console.log('Request from ' + req.ip);
-    next();
-});
+var port = process.env.port || 5000
 
-app.get('/',function(req,res){
-    res.send('Hello World!');
-});
+require('./config/express')(app, config);
 
-app.get('/about',function(req,res){
-    res.send('About Us!!');
+require('http').createServer(app).listen(config.port, function () {
+    console.log("HTTP Server listening on port: %d, in %s mode", config.port, app.get('env'));
 });
-app.get('/about/directions',function(req,res){
-    res.send('How to find Us!');
-});
-
-app.use(function(req, res){       //THESE TWO SHOUL BE AT THE END.. 404 and 500 errors
-    res.type('text/plan');
-    res.status(404);
-    res.send('404 Not Found');
-});
-app.use(function(err, req, res, next){
-    console.error(err.stack);
-    res.type('text/plan');
-    res.status(500);
-    res.send('500 Sever Error');
-});
-http.createServer(app).listen(3000, function(){
-    console.log('Express server listening on port ' + 3000);
-})
